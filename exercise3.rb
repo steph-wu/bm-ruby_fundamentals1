2adfd4ba5e5fa6d7408db9d8027c8b5788344ec8
STEPHANIE <WU.STEPHANIE@LIVE.COM>
2017-01-16 12:38:11 -0500

    Added request for user's ae and returned their year of birth

--- exercise3.rb
+++ exercise3.rb
@@ -1,3 +1,10 @@
 puts "What is your name?"
 name = gets.chomp
 puts "Hi #{name}!"
+
+puts "What is your age?"
+age = gets.chomp
+
+birthdate = 2017 - age.to_i
+
+puts "You were born in #{birthdate}."
