e0e5c11745d3e034fdd3943f950539a486730a47
STEPHANIE <WU.STEPHANIE@LIVE.COM>
2017-01-16 12:31:56 -0500

    Completed exercise 2

---
+++ exercise2.rb
@@ -0,0 +1,18 @@
+#1 How would you calculate a good tip for a 55 dollar meal?
+
+puts 55 * 1.18
+
+#2 Try adding a string and an integer with the + operator
+
+puts 5.to_s + " cats"
+
+#3 Try outputting the result of 45628 multiplied by 7838 in a sentence by using string interpolation
+
+puts "45628 x 7839 = #{45628 * 7839}"
+
+#4 What's the value of the expression (10 < 20 && 30 < 20) || !(10 == 1) }
+  # 10 < 20 = true, 30 < 20 = false
+  # !(10 == 1) = true
+  # The expression is true
+
+puts (10 < 20 && 30 < 20) || !(10 == 1)
